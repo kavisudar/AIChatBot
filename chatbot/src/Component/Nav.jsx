102acae7e006b2f77aa009b3e2a8581de5d3ed9d
kavisudar <kavisudar6@gmail.com>
2025-04-23 16:22:16 +0530

    components created

---
+++ chatbot/src/Component/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css';
+function Nav() {
+  return (
+    <>
+    <div className='nav'>
+        <h1>Jarvis</h1>
+        <h6>Explore the AI Feature</h6>
+        <h2>Welcome </h2>
+    </div>
+    </>
+  )
+}
+
+export default Nav;
